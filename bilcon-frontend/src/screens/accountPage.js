6553e21d8882482ebe51cdac4451b4493f69848d
Gokalp Gokdogan <gokalp.gokdogan@gmail.com>
2023-11-15 20:44:15 +0300

    cooler account ~ali

--- bilcon-frontend/src/screens/accountPage.js
+++ bilcon-frontend/src/screens/accountPage.js
@@ -50,18 +50,19 @@ function AccountPage()
             <div className='flex flex-row'>
         <div className='flex flex-col bg-white h-screen px-10'>
             <p className='font-inter font-extrabold text-3xl text-ui-purple my-4'>Account</p>
-            <Link type="submit" to="/home" className={buttonClassAccount}>Security</Link>
-            <Link type="submit" to="/home" className={buttonClassAccount}>Notifications</Link>
+            <Link type="submit" to="/home" className={buttonClassAccount}>Favorites</Link>
             <Link type="submit" to="/home" className={buttonClassAccount}>Purchases</Link>
-            <Link type="submit" to="/home" className={buttonClassAccount}>Sold Items</Link>
+            <Link type="submit" to="/home" className={buttonClassAccount}>Notifications</Link>
             
+            
+            <Link type="submit" to="/home" className={buttonClassAccount}>Security</Link>
+            <Link type="submit" to="/home" className={buttonClassAccount}>Sold Items</Link>
+            <Link type="submit" to="/home" className={buttonClassAccount}>Ratings</Link>
             <div className='flex flex-row justify-center'>
-                <Link type="submit" to="/home" className={buttonClassAccount}>Ratings</Link>
+                
                 {starIcons}
             </div>
-            
-            <Link type="submit" to="/home" className={buttonClassAccount}>Favorites</Link>
-            
+
             <div className='flex flex-col w-64 mt-3'>
                 
             </div>
